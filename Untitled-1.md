4e279ebc00905e3ad804cddf51d753503d987f8a
Graeham Rieman <graeham.rieman@gmail.com>
2017-05-05 01:09:12 -0500

    Python processing

--- Untitled-1.md
+++ Untitled-1.md
@@ -13,49 +13,52 @@ Seeking to emulate this and pick up some new python skills, I tried to do an ana
 
 ### Calculating Differences
 
-```python
+As NASA provided eight subimages, I used those to make the processes less memory intensive while maintaining a high image quality. 
 
-```
-
-### Combine Images
+The first step was to find which pixels changed between the two images, and quantify that change. I wanted the end result to be an image - with green values denoting brighter pixels and blue darker. Loading the images with Python Imaging Library (PIL), they were converted to numpy arrays and the difference between them was calculated. Any negative values were set to zeros. Lastly, the two arrays were combined with an array of zeros to make a color image which was then saved.
 
 ```python
-def merge_images(square):
-    image1 = Image.open(square.join(("diff\\BlackMarble_diff_","_1_color.jpg")))
-    image2 = Image.open(square.join(("diff\\BlackMarble_diff_","_3_color.jpg")))
-    image3 = Image.open(square.join(("diff\\BlackMarble_diff_","_2_color.jpg")))
-    image4 = Image.open(square.join(("diff\\BlackMarble_diff_","_4_color.jpg")))
+from PIL import Image
+import numpy as np
+import math
 
-    (width1, height1) = image1.size
-    (width2, height2) = image2.size
+def bg_changes(square):
+    im12 = Image.open(square.join(("2012\\BlackMarble_2012_","_gray.jpg")))
+    im16 = Image.open(square.join(("2016\\BlackMarble_2016_","_gray.jpg")))
+    dat12 = np.array(im12).astype('int32')
+    dat16 = np.array(im16).astype('int32')
+    new = dat16 - dat12
+    off = dat12 - dat16
+    new[new < 0] = 0
+    off[off < 0] = 0
+    
+    out = np.concatenate((np.zeros((21600,21600))[..., np.newaxis], new[...,np.newaxis], off[...,np.newaxis]), axis=2)
+    out = out.astype('uint8')
+    out = Image.fromarray(out)
+    out.save(square.join(("diff\\",".jpg")))
 
-    result_width = width1 + width2
-    result_height = height1 + height2
-
-    result = Image.new('RGB', (result_width, result_height))
-    result.paste(im=image1, box=(0, 0))
-    result.paste(im=image2, box=(width1, 0))
-    result.paste(im=image3, box=(0, height1))
-    result.paste(im=image4, box=(width1, height1))
-    result.save(square.join(("diff\\BlackMarble_diff_","_color.jpg")))
-
-
-merge_images(square = "A1")
-merge_images(square = "A2")
-merge_images(square = "B1")
-merge_images(square = "B2")
-merge_images(square = "C1")
-merge_images(square = "C2")
-merge_images(square = "D1")
-merge_images(square = "D2")
+bg_changes(square = "A1")
+bg_changes(square = "A2")
+bg_changes(square = "B1")
+bg_changes(square = "B2")
+bg_changes(square = "C1")
+bg_changes(square = "C2")
+bg_changes(square = "D1")
+bg_changes(square = "D2")
 ```
 
-### Paste over Image
+![](/images/BlackMarble/diff_C1.jpg)
+
+### Making the Data Recognizable
+
+This image has all the desired information, but with only lights it's not immediately apparent which cities and places are which without a substantial knowledge of geography. To make it easier, I overlaid the image from above onto the other image provided by NASA, with some topographical images.
+
+In my opinion, is a bit less informative than the previous version, since some of the changes were blurred out during image processing or hard to identify against the color of the background. 
 
 ```python
 def overlay_images(square):
-	bg = Image.open(square.join(("backgounds\\BlackMarble_2016_",".jpg")).convert("RGBA")
-	fg = Image.open(square.join(("diff\\BlackMarble_diff_","_color.jpg"))
+	bg = Image.open(square.join(("backgrounds\\BlackMarble_2016_",".jpg"))).convert("RGBA")
+	fg = Image.open(square.join(("diff\\",".jpg")))
 	
 	mask = fg.convert('L')
 	maskdat = np.array(mask)
@@ -64,11 +67,9 @@ def overlay_images(square):
 	
 	imgdata = np.array(fg.convert("RGBA"))
 	imgdata[:,:,3] = maskdat
-	foreground = Image.fromarray(data)
+	foreground = Image.fromarray(imgdata)
 	out = Image.alpha_composite(bg, foreground)
-	out.save(square.join(("out\\",".jpg"))
-
-
+	out.save(square.join(("out\\",".jpg")))
 
 overlay_images(square = "A1")
 overlay_images(square = "A2")
@@ -78,4 +79,35 @@ overlay_images(square = "C1")
 overlay_images(square = "C2")
 overlay_images(square = "D1")
 overlay_images(square = "D2")
+```
+![](/images/BlackMarble/background_C1.jpg)
+
+### Combine Images
+
+```python
+imageA1 = Image.open("diff\\A1.jpg")
+imageA2 = Image.open("diff\\A2.jpg")
+imageB1 = Image.open("diff\\B1.jpg")
+imageB2 = Image.open("diff\\B2.jpg")
+imageC1 = Image.open("diff\\C1.jpg")
+imageC2 = Image.open("diff\\C2.jpg")
+imageD1 = Image.open("diff\\D1.jpg")
+imageD2 = Image.open("diff\\D2.jpg")
+
+(width, height) = imageA1.size
+
+result_width = width * 4
+result_height = height * 2
+
+result = Image.new('RGB', (result_width, result_height))
+result.paste(im=imageA1, box=(0, 0))
+result.paste(im=imageA2, box=(0, height))
+result.paste(im=imageB1, box=(width, 0))
+result.paste(im=imageB2, box=(width, height))
+result.paste(im=imageC1, box=(width*2, 0))
+result.paste(im=imageC2, box=(width*2, height))
+result.paste(im=imageD1, box=(width*3, 0))
+result.paste(im=imageD2, box=(width*3, height))
+
+result.save("diff\\merged.jpg")
 ```
--- scratch.py
+++ scratch.py
@@ -2,23 +2,152 @@ from PIL import Image
 import numpy as np
 import math
 
-im12 = Image.open("2012.jpg")
-dat12 = np.array(im12)
-im16 = Image.open("2016.jpg")
-dat16 = np.array(im16)
-new = dat16 - dat12
-off = dat12 - dat16
-new[new < 0] = 0
-off[off < 0] = 0
+def bg_changes(square):
+    im12 = Image.open(square.join(("2012\\BlackMarble_2012_","_gray.jpg")))
+    im16 = Image.open(square.join(("2016\\BlackMarble_2016_","_gray.jpg")))
+    dat12 = np.array(im12).astype('int32')
+    dat16 = np.array(im16).astype('int32')
+    new = dat16 - dat12
+    off = dat12 - dat16
+    new[new < 0] = 0
+    off[off < 0] = 0
+    
+    out = np.concatenate((np.zeros((21600,21600))[..., np.newaxis], new[...,np.newaxis], off[...,np.newaxis]), axis=2)
+    out = out.astype('uint8')
+    out = Image.fromarray(out)
+    out.save(square.join(("diff\\",".jpg")))
 
-out = np.concatenate((np.zeros((21600,21600))[..., np.newaxis], new[...,np.newaxis], off[...,np.newaxis]), axis=2)
+bg_changes(square = "A1")
+bg_changes(square = "A2")
+bg_changes(square = "B1")
+bg_changes(square = "B2")
+bg_changes(square = "C1")
+bg_changes(square = "C2")
+bg_changes(square = "D1")
+bg_changes(square = "D2")
+
+
+#def overlay_images(square):
+#	bg = Image.open(square.join(("backgrounds\\BlackMarble_2016_",".jpg"))).convert("RGBA")
+#	fg = Image.open(square.join(("diff\\",".jpg")))
+#	
+#	im16 = Image.open(square.join(("2016\\BlackMarble_2016_","_gray.jpg")))
+#	
+#	fgdat = np.array(fg).astype('int32')
+#	r,g,b = np.split(fgdat,3,2)
+#	maskdat = g+b
+#	maskdat = np.floor((maskdat[:,:,0] / 255)+.9) * 255
+#	maskdat = maskdat.astype('uint8')
+#	
+#	imgdata = np.array(fg.convert("RGBA"))
+#	imgdata[:,:,3] = maskdat
+#	foreground = Image.fromarray(imgdata)
+#	#out = Image.alpha_composite(bg, foreground)
+#	bg.paste(foreground, (0,0), foreground)
+#	out.save(square.join(("out\\",".jpg")))
+
+
+def overlay_images(square):
+	bg = Image.open(square.join(("backgrounds\\BlackMarble_2016_",".jpg"))).convert("RGBA")
+	fg = Image.open(square.join(("diff\\",".jpg")))
+	
+	mask = fg.convert('L')
+	maskdat = np.array(mask)
+	maskdat = np.floor((maskdat / 255)+.99) * 255
+	maskdat = maskdat.astype('uint8')
+	
+	imgdata = np.array(fg.convert("RGBA"))
+	imgdata[:,:,3] = maskdat
+	foreground = Image.fromarray(imgdata)
+	out = Image.alpha_composite(bg, foreground)
+	out.save(square.join(("out\\",".jpg")))
+
+overlay_images(square = "A1")
+overlay_images(square = "A2")
+overlay_images(square = "B1")
+overlay_images(square = "B2")
+overlay_images(square = "C1")
+overlay_images(square = "C2")
+overlay_images(square = "D1")
+overlay_images(square = "D2")
+
+
+imageA1 = Image.open("diff\\A1.jpg")
+imageA2 = Image.open("diff\\A2.jpg")
+imageB1 = Image.open("diff\\B1.jpg")
+imageB2 = Image.open("diff\\B2.jpg")
+imageC1 = Image.open("diff\\C1.jpg")
+imageC2 = Image.open("diff\\C2.jpg")
+imageD1 = Image.open("diff\\D1.jpg")
+imageD2 = Image.open("diff\\D2.jpg")
+
+(width, height) = imageA1.size
+
+result_width = width * 4
+result_height = height * 2
+
+result = Image.new('RGB', (result_width, result_height))
+result.paste(im=imageA1, box=(0, 0))
+result.paste(im=imageA2, box=(0, height))
+result.paste(im=imageB1, box=(width, 0))
+result.paste(im=imageB2, box=(width, height))
+result.paste(im=imageC1, box=(width*2, 0))
+result.paste(im=imageC2, box=(width*2, height))
+result.paste(im=imageD1, box=(width*3, 0))
+result.paste(im=imageD2, box=(width*3, height))
+
+result.save(square.join(("diff\\merged.jpg")))
+
+
+
+
+
+
+
+
+bg = Image.open(square.join(("backgrounds\\BlackMarble_2016_",".jpg"))).convert("RGBA")
+fg = Image.open(square.join(("diff\\",".jpg")))
+out = bg
+
+#fgdat = np.array(fg).astype('int32')
+#r,g,b = np.split(fgdat,3,2)
+#maskdat = g[:,:,0] + b[:,:,0]
+#maskdat = np.floor((maskdat / 255)+.995) * 255
+	
+fgdat = np.array(fg.convert("1"))
+	
+#im16 = Image.open(square.join(("2016\\BlackMarble_2016_","_gray.jpg")))
+#dat16 = np.array(im16).astype('int32')
+#dat16 = np.floor((dat16 / 255)+.9) * 255
+
+imgdata = np.array(fg.convert("RGBA"))
+imgdata[:,:,3] = fgdat.astype('uint8')
+foreground = Image.fromarray(imgdata)
+#out = Image.alpha_composite(bg, foreground)
+out.paste(foreground, (0,0), foreground)
+out.save(square.join(("out\\",".jpg")))
+
+
+
+
+#im12 = Image.open("2012.jpg").convert('L')
+#dat12 = np.array(im12).astype('int32')
+#im16 = Image.open("2016.jpg").convert('L')
+#dat16 = np.array(im16).astype('int32')
+#new = dat16 - dat12
+#off = dat12 - dat16
+#new[new < 0] = 0
+#off[off < 0] = 0
+#del dat12; del dat16; del im12; del im16
+
+#out = np.concatenate((np.zeros((21600,21600))[..., np.newaxis], new[...,np.newaxis], off[...,np.newaxis]), axis=2)
+#out = out.astype('uint8')
+#out = Image.fromarray(out)
+#out.save("test.jpg")
+
 
 def merge_images(square):
-    """Merge two images into one, displayed side by side
-    :param file1: path to first image file
-    :param file2: path to second image file
-    :return: the merged Image object
-    """
+
     image1 = Image.open(square.join(("diff\\BlackMarble_diff_","_1_color.jpg")))
     image2 = Image.open(square.join(("diff\\BlackMarble_diff_","_3_color.jpg")))
     image3 = Image.open(square.join(("diff\\BlackMarble_diff_","_2_color.jpg")))
